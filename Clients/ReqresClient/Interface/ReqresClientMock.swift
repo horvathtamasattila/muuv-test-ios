0498bdad8433cda5b267d3b90af0c36ce7b973df
Horvath Tamas <tamas.horvath@makery.co>
2022-08-15 13:55:31 +0100

    chore: add local json for mock environment

--- Clients/ReqresClient/Interface/ReqresClientMock.swift
+++ Clients/ReqresClient/Interface/ReqresClientMock.swift
@@ -4,7 +4,19 @@ import Foundation
 public extension ReqresClient {
     static var mock: Self {
         return Self(
-            getUserList: { _ in Result.Publisher([]).eraseToAnyPublisher() }
+            getUserList: { _ in
+                let resource = "get_users_200"
+                let decoder = JSONDecoder()
+                do {
+                    let url = Bundle.main.url(forResource: resource, withExtension: "json")
+                    let data = try Data(contentsOf: url!)
+                    let result = try decoder.decode(ReqresResult.self, from: data)
+                    return Result.Publisher(result.data).eraseToAnyPublisher()
+                } catch {
+                    print(error)
+                    return Result.Publisher([]).eraseToAnyPublisher()
+                }
+            }
         )
     }
 }
